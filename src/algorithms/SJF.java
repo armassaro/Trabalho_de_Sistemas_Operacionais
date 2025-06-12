1d7134e18f5558ef563521262f94f44403eb2208
armassaro <132797747+armassaro@users.noreply.github.com>
2025-06-12 11:26:23 -0300

    Update SJF.java

--- src/algorithms/SJF.java
+++ src/algorithms/SJF.java
@@ -1,48 +1,88 @@
 package algorithms;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class SJF {
-    private Queue<Process> runtimeProcessesQueue = new LinkedList<>();
+    private List<Process> runtimeProcessesList = new LinkedList<>();
     private List<Process> allProcessesList = new ArrayList<>();
     private List<Process> finishedProcessesList = new ArrayList<>();
     private int totalTime = 0;
     private String timeline = "";
     private boolean showDialogues, fastExecutionMode;
-
+    private Process executingProcess = null;
+    
     public SJF(boolean showDialogues, boolean fastExecutionMode) { 
         this.showDialogues = showDialogues;
         this.fastExecutionMode = fastExecutionMode;
     }
 
     public void run() { 
-        // Simula passagem de tempo
-        if(fastExecutionMode) { 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
+        int allProcessesListSize = allProcessesList.size();
         
+        while(finishedProcessesList.size() < allProcessesListSize) { 
+            // Simula passagem de tempo
+            if(fastExecutionMode) { 
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            executingProcess = checkForShortestJob();
+            
+            // Move o próximo processo para a fila de execução
+            moveArrivingProcessToRuntimeList();
+            
+            if(showDialogues) {
+                System.out.println("\n====================\n");
+                System.out.println("Processo em execução: " + executingProcess.getId());
+                System.out.println("Tempo restante do processo: " + executingProcess.getProcessTime());
+                System.out.println("Tempo de espera do processo: " + executingProcess.getWaitingTime());
+                System.out.println("Tempo total: " + totalTime);
+            }
+            
+            totalTime++;
+        }
     }
 
-    private void checkForShortestJob() { 
-
+    private Process checkForShortestJob() { 
+        // Pega o menor tempo dos processos disponíveis
+        int shortestJobTime = (executingProcess == null ? 0 : executingProcess.getProcessTime());
+        
+        for(Process p : runtimeProcessesList) { 
+            if(p.getProcessTime() < shortestJobTime) { 
+                return p;
+            }
+        }
+        
+        // Caso passar pela runtimeQueue e não encontrar um processo com tempo menor que o processo com menor tempo, então retorna o próprio executingProcess
+        return executingProcess;
+    }
+    
+    // Move o próximo processo para a lista de processos em execução, ordenando os mesmos conforme tempo de processamento
+    private void moveArrivingProcessToRuntimeList() {
+        Iterator<Process> runtimeProcessesListIterator = runtimeProcessesList.iterator();
+        
+        for (Process p : allProcessesList) {
+            if(p.getArrivalTime() == totalTime) {
+                
+            }
+        }
     }
 
     // ================= Getters e Setters =================
 
-     public Queue<Process> getRuntimeQueue() {
-        return runtimeProcessesQueue;
+     public List<Process> getRuntimeList() {
+        return runtimeProcessesList;
     }
 
-    public void setRuntimeQueue(Queue<Process> runtimeProcessesQueue) {
-        this.runtimeProcessesQueue = runtimeProcessesQueue;
+    public void setRuntimeList(List<Process> runtimeProcessesList) {
+        this.runtimeProcessesList = runtimeProcessesList;
     }
 
     public List<Process> getAllProcessesList() {
@@ -53,8 +93,8 @@ public class SJF {
         this.allProcessesList = allProcessesList;
     }
 
-    public void addProcessToRuntimeQueue(Process process) {
-        this.runtimeProcessesQueue.add(process);
+    public void addProcessToRuntimeList(Process process) {
+        this.runtimeProcessesList.add(process);
     }
 
     public void addProcessToAllProcessesList(Process process) {
@@ -73,7 +113,6 @@ public class SJF {
             System.out.println("ID do processo: " + p.getId());
             System.out.println("Tempo de processamento restante: " + p.getProcessTime());
             System.out.println("Tempo de chegada do processo: " + p.getArrivalTime());
-            System.out.println("Tempo de espera do processo: " + p.getWaitingTime());
         }
     }
 }
